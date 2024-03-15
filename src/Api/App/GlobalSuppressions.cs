868d7f6ffe7774a171903848858a53fff6c43663
hmsiegel <hmsiegel@gmail.com>
2024-03-15 07:18:04 -0400

    Adds Docker Support
    The most significant changes include the addition of Docker and Docker Compose configurations, changes to the `.editorconfig` and `Directory.Build.props` files to modify code analysis rules, and the addition of a new project to the `WLCS.sln` solution.
    
    1. Docker and Docker Compose configurations were added. This includes changes to `.dockerignore` to ignore certain files and directories, the addition of Docker-related properties and a package reference in `App.csproj`, and the creation of a Docker Compose project with a Docker Compose override file and a Dockerfile. In `docker-compose.yml`, services were defined for the application, a PostgreSQL database, and a pgAdmin4 database management tool. A new profile for Docker Compose was added to `launchSettings.json`, and a multi-stage build process was defined in `Dockerfile` for the application.
    
    2. The `.editorconfig` file was modified to remove the severity of the SA1633 rule and add the IDE0008 rule, which enforces the use of explicit types instead of the `var` keyword. This file was also removed from the `Modules.Meets.IntegrationEvents.csproj` project.
    
    3. In `Directory.Build.props`, the properties `TreatWarningsAsErrors` and `CodeAnalysisTreatWarningsAsErrors` were changed from `true` to `false`, meaning that warnings will no longer be treated as errors.
    
    4. A new project was added to the `WLCS.sln` solution, and the `.gitignore` file was added to the solution items. A new project configuration was added for this new project.
    
    5. In `GlobalSuppressions.cs`, suppressions were added for two StyleCop rules, and in `GlobalUsings.cs`, a global using directive was added for `System.Diagnostics.CodeAnalysis`.
    
    6. The `.gitignore` file was updated to ignore the `.containers/` directory.
    
    7. The configuration for the `http` and `https` profiles in `launchSettings.json` was changed, and a new profile was added for Docker.

---
+++ src/Api/App/GlobalSuppressions.cs
@@ -0,0 +1,2 @@
+﻿[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "Reviewed")]
+[assembly: SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1200:Using directives should be placed correctly", Justification = "Reviewed")]

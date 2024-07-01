73f547857863a9639b808a97d866d7f194e3cecd
hmsiegel <hmsiegel@gmail.com>
2024-07-01 07:19:15 -0400

    Add Unit Test for Competition Module
    - Introduced `BaseTest.cs` as an abstract base class for all unit tests in the `WLCS.Modules.Competition.UnitTests.Abstractions` namespace, including a static `Faker` instance and a method for asserting domain events.
    - Added `GlobalUsings.cs` to globally import common namespaces, simplifying the project's codebase.
    - Created `MeetTests.cs` under `WLCS.Modules.Competition.UnitTests.Meets` with tests for the `Meet` class, ensuring proper behavior in domain event publishing and instance creation validation.
    - Updated `WLCS.Modules.Competition.UnitTests.csproj` with new package references (`Bogus`, `FluentAssertions`, etc.), a project reference to `WLCS.Modules.Competition.Domain`, and configuration as a non-packable test project.

---
+++ test/Modules/Competition/WLCS.Modules.Competition.UnitTests/GlobalUsings.cs
@@ -0,0 +1,12 @@
+﻿// <copyright file="GlobalUsings.cs" company="WLCS">
+// Copyright (c) WLCS. All rights reserved.
+// </copyright>
+global using Bogus;
+
+global using FluentAssertions;
+
+global using NodaTime;
+
+global using WLCS.Common.Domain;
+global using WLCS.Modules.Competition.Domain.Meets;
+global using WLCS.Modules.Competition.Domain.Meets.DomainEvents;
---
+++ test/Modules/Competition/WLCS.Modules.Competition.UnitTests/Meets/MeetTests.cs
@@ -0,0 +1,58 @@
+﻿// <copyright file="MeetTests.cs" company="WLCS">
+// Copyright (c) WLCS. All rights reserved.
+// </copyright>
+
+using WLCS.Modules.Competition.UnitTests.Abstractions;
+
+namespace WLCS.Modules.Competition.UnitTests.Meets;
+
+/// <summary>
+/// Tests for the <see cref="Meet"/> class.
+/// </summary>
+public class MeetTests : BaseTest
+{
+  /// <summary>
+  /// Asserts that the end date should not precede the start date.
+  /// </summary>
+  [Fact]
+  public void Create_ShouldReturnFailure_WhenEndDatePrecedesStartDate()
+  {
+    // Arrange
+    var startsAtUtc = LocalDate.FromDateOnly(DateOnly.FromDateTime(DateTime.Now));
+
+    // Act
+    var meet = Meet.Create(
+      Faker.Company.CompanyName(),
+      Faker.Address.City(),
+      Faker.Company.CompanyName(),
+      startsAtUtc,
+      startsAtUtc.PlusDays(-1));
+
+    // Assert
+    meet.Error.Should().Be(MeetErrors.EndDatePrecedesStartDate);
+  }
+
+  /// <summary>
+  /// Asserts that a domain event is raised when a meet is created.
+  /// </summary>
+  [Fact]
+  public void Create_ShouldRaiseDomainEvent_WnenMeetIsCreated()
+  {
+    // Arrange
+    var startsAtUtc = LocalDate.FromDateOnly(DateOnly.FromDateTime(DateTime.Now));
+
+    // Act
+    var result = Meet.Create(
+      Faker.Company.CompanyName(),
+      Faker.Address.City(),
+      Faker.Company.CompanyName(),
+      startsAtUtc,
+      startsAtUtc);
+
+    var meet = result.Value;
+
+    // Assert
+    var domainEvent = AssertDomainEventWasPublished<MeetCreatedDomainEvent>(meet);
+    domainEvent.MeetId.Should().Be(meet.Id);
+  }
+}
